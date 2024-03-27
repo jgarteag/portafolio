bea6a47fb44e69d5419b881c6867c83ef797de66
Juan Manuel Guerrero Arteaga <78865058+juanmgart92@users.noreply.github.com>
2024-03-27 01:19:19 -0500

    Remove build.bat and public directory

--- build.bat
+++
@@ -1,9 +0,0 @@
-python -m venv .venv
-.venv\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-reflex init
-reflex export --frontend-only
-expand frontend.zip -f:* public
-del /f frontend.zip
-deactivate
